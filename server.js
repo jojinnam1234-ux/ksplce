be8a9a7042341f396118039b3f4aa91484d8c63e
unknown <jojinnam1234@gmail.com>
2026-01-30 09:59:41 +0900

    connect pstgres db

--- server.js
+++ server.js
@@ -1,57 +1,50 @@
 const express = require("express")
-const mysql = require("mysql2")
 const bcrypt = require("bcrypt")
+const { Pool } = require("pg")
 
 const app = express()
 app.use(express.json())
 app.use(express.static("public"))
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "ksplce!@#",
-    database: "mysite"
+const db = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
 })
 
-// 회원가입
+
 app.post("/signup", async (req, res) => {
     const { email, password } = req.body
     const hash = await bcrypt.hash(password, 10)
 
-    db.query(
-        "INSERT INTO users(email,password) VALUES (?,?)",
-        [email, hash],
-        err => {
-            if (err) return res.send("이미 존재")
-            res.send("회원가입 성공")
-        }
-    )
+    try {
+        await db.query(
+            "INSERT INTO users(email, password) VALUES ($1, $2)",
+            [email, hash]
+        )
+        res.send("회원가입 성공")
+    } catch {
+        res.send("이미 존재")
+    }
 })
 
-// 로그인
-app.post("/login", (req, res) => {
+
+app.post("/login", async (req, res) => {
     const { email, password } = req.body
 
-    db.query(
-        "SELECT * FROM users WHERE email=?",
-        [email],
-        async (err, result) => {
-            if (result.length === 0) return res.send("없음")
-
-            const ok = await bcrypt.compare(password, result[0].password)
-            if (ok) res.send("로그인 성공")
-            else res.send("비번 틀림")
-        }
+    const result = await db.query(
+        "SELECT * FROM users WHERE email=$1",
+        [email]
     )
-})
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+    if (result.rows.length === 0) return res.send("없음")
 
-const PORT = process.env.PORT || 3000
+    const ok = await bcrypt.compare(password, result.rows[0].password)
 
-app.listen(PORT, () => {
-    console.log("Server running on", PORT)
+    if (ok) res.send("로그인 성공")
+    else res.send("비번 틀림")
 })
 
